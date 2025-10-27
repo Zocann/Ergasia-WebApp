79addbc511235ea367f7bc9efbff8fa35ca5ec1f
Karel Mareth <mareth.karel@gmail.com>
2025-10-27 08:15:07 +0100

    jquery 5

--- wwwroot/js/reg-validation.js
+++ wwwroot/js/reg-validation.js
@@ -1,42 +1,45 @@
-$(function(){
-    $("#password").keydown(function()
-    {
+$(function () {
+    $("#password").keydown(function () {
         let password = $(this).val();
         let regCount = $("#reg-count");
         let passwordCounter = 0;
-        
-        if(password.length < 8)
-        {
+
+        if (password.length < 8) {
+            regCount.show();
             regCount.attr("class", "invalid-feedback");
             regCount.text("Please enter " + (8 - password.length) + " more characters");
-        } else
-        {
+        } else {
             regCount.hide();
             passwordCounter += 1;
         }
-        
-        if (hasLowerCase(password)) $("reg-low").hide();
-        else passwordCounter += 1;
-        
-        if (hasUpperCase(password)) $("reg-up").hide();
-        else passwordCounter += 1;
-        
-        if (hasSpecialCharacter(password)) $("reg-down").hide();
-        else passwordCounter += 1;
-        
-        if (hasNumber(password)) $("reg-num").hide();
-        else passwordCounter += 1;
-        
-        if (passwordCounter === 5)
-        {
+
+        if (hasLowerCase(password)) {
+            $("reg-low").hide();
+            passwordCounter += 1;
+        } else $("reg-low").show();
+
+        if (hasUpperCase(password)) {
+            $("reg-up").hide();
+            passwordCounter += 1;
+        } else $("reg-up").show();
+
+        if (hasSpecialCharacter(password)) {
+            $("reg-spec").hide();
+            passwordCounter += 1;
+        } else $("reg-spec").show();
+
+        if (hasNumber(password)) {
+            $("reg-num").hide();
+            passwordCounter += 1;
+        } else $("reg-num").show();
+
+        if (passwordCounter === 5) {
             $("password").attr("class", "form-control mb-3 is-valid");
-        }
-        else
-        {
+        } else {
             $("password").attr("class", "form-control mb-3 is-invalid");
         }
     });
-    
+
 });
 
 function hasLowerCase(str) {
