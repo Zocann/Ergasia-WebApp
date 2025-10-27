42f89dfdbc35da44b734f79bb2931e6815ed6409
Karel Mareth <mareth.karel@gmail.com>
2025-10-27 08:45:13 +0100

    final jquery

--- wwwroot/js/reg-validation.js
+++ wwwroot/js/reg-validation.js
@@ -20,24 +20,24 @@ $(function () {
 
 
         if (hasLowercase) {
-            $("reg-low").hide();
+            $("#reg-low").hide();
             passwordCounter += 1;
-        } else $("reg-low").show();
+        } else $("#reg-low").show();
 
         if (hasUppercase) {
-            $("reg-up").hide();
+            $("#reg-up").hide();
             passwordCounter += 1;
-        } else $("reg-up").show();
+        } else $("#reg-up").show();
 
         if (hasSpecial) {
-            $("reg-spec").hide();
+            $("#reg-spec").hide();
             passwordCounter += 1;
-        } else $("reg-spec").show();
+        } else $("#reg-spec").show();
 
         if (hasNumeric) {
-            $("reg-num").hide();
+            $("#reg-num").hide();
             passwordCounter += 1;
-        } else $("reg-num").show();
+        } else $("#reg-num").show();
 
         if (passwordCounter === 5) {
             $("password").attr("class", "form-control mb-3 is-valid");
