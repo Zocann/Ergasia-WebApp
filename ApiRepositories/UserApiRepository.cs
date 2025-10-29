706d6269f80750e2b475a44276877bc5c6e1e875
Karel Mareth <mareth.karel@gmail.com>
2025-10-29 13:03:16 +0100

    refactoring

--- ApiRepositories/UserApiRepository.cs
+++ ApiRepositories/UserApiRepository.cs
@@ -8,12 +8,9 @@ using Ergasia_WebApp.Services;
 
 namespace Ergasia_WebApp.ApiRepositories;
 
-public class UserApiRepository(IHttpContextAccessor contextAccessor, IHttpClientFactory clientFactory)
+public class UserApiRepository(IHttpClientFactory clientFactory)
     : IUserApiRepository
 {
-    public required HttpContext Context =
-        contextAccessor.HttpContext ?? throw new InvalidOperationException("No HttpContextAccessor");
-
     private readonly HttpClient _client = clientFactory.CreateClient("API");
 
     public async Task<UserDto?> GetAsync(string userId, string accessToken)
@@ -116,7 +113,9 @@ public class UserApiRepository(IHttpContextAccessor contextAccessor, IHttpClient
 
     private bool ManageResponse(HttpResponseMessage response)
     {
-        if (response.StatusCode == HttpStatusCode.Unauthorized) Context.Response.StatusCode = 401;
+        var httpContextAccessor = new HttpContextAccessor();
+        if (httpContextAccessor.HttpContext == null) throw new ApplicationException("No context accessor");
+        if (response.StatusCode == HttpStatusCode.Unauthorized) httpContextAccessor.HttpContext.Response.StatusCode = 401;
         return response.IsSuccessStatusCode;
     }
 }
