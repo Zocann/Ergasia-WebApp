b430dea8fb4afc8268a407087a59309694345d09
Karel Mareth <mareth.karel@gmail.com>
2025-11-07 10:35:38 +0100

    Changes

--- Data/ClientData.cs
+++ Data/ClientData.cs
@@ -3,6 +3,6 @@ namespace Ergasia_WebApp.Data;
 public class ClientData(IHttpContextAccessor httpContextAccessor)
 {
     public string? Id { get; private set; } = httpContextAccessor.HttpContext?.Request.Cookies["userId"];
-    public string? Role { get; private set; } = httpContextAccessor.HttpContext?.Request.Cookies["role"];
+    public string? Role { get; private set; } = httpContextAccessor.HttpContext?.Request.Cookies["userRole"];
     public string? AccessToken { get; private set; } = httpContextAccessor.HttpContext?.Request.Cookies["accessToken"];
 }
