ccfd72cc0385bc9601cf4daf82cedbf534ea2c4b
Karel Mareth <mareth.karel@gmail.com>
2025-11-07 10:39:34 +0100

    Another changes

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -9,13 +9,13 @@ namespace Ergasia_WebApp.Pages;
 public class IndexModel(IJobApiRepository jobApiRepository, IRatingApiRepository ratingApiRepository) : PageModel
 {
     public List<JobDto> Jobs { get; set; } = [];
-    public Dictionary<string, decimal?> AverageRatings { get; set; } = new();
+    public Dictionary<string, decimal> AverageRatings { get; set; } = new();
     public string? Error;
     
     
     public async Task<IActionResult> OnGetAsync(string? error)
     {
-        Dictionary<string, decimal?> allAverageRatings = new();
+        Dictionary<string, decimal> allAverageRatings = new();
         var result = await jobApiRepository.GetAllUpcomingAsync();
         
         if (result != null)
@@ -30,13 +30,17 @@ public class IndexModel(IJobApiRepository jobApiRepository, IRatingApiRepository
                 var averageRating = await ratingApiRepository.GetEmployerAverageRating(job.EmployerId);
                 if (averageRating == null) continue;
 
-                allAverageRatings.Add(job.Id, averageRating);
+                allAverageRatings.Add(job.Id, (decimal)averageRating);
             }
 
             //Take only 4 best rated employers
-            AverageRatings = allAverageRatings.OrderBy(ar => ar.Value).Take(4).ToDictionary();
+            AverageRatings = allAverageRatings.OrderByDescending(ar => ar.Value).Take(4).ToDictionary();
 
-            Jobs = jobs.TakeWhile(j => j.Id != null && AverageRatings.ContainsKey(j.Id)).ToList();
+            foreach (var averageRating in AverageRatings)
+            {
+                var job = jobs.FirstOrDefault(j => j.Id == averageRating.Key);
+                if (job != null) Jobs.Add(job);
+            }
         }
 
         Error = error;
